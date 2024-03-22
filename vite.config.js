230c0c957e55f8ee5f46935536cd3c350dd31e25
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-03-22 18:26:26 +0530

    image optimize

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vue from '@vitejs/plugin-vue';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vue()],
+  plugins: [react(),
+  vue(),
+
+  ViteImageOptimizer({
+    png: {
+      quality: 50,
+    },
+    jpg: {
+      quality: 50,
+    },
+  }),],
   // base :"https://cutechsolutions.co.uk/"
   base: "https://cutechsolution.onrender.com/",
 })
