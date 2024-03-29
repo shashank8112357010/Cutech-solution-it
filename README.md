362565f7b770360691ff163a1abd3def5b6d0ea7
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-03-29 20:17:57 +0530

    removed main

--- README.md
+++
@@ -1,3 +0,0 @@
-# Cutechsolution
-
-Hey
--- src/routes/NoPageFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NoPageFound = () => {
-    return (
-        <div>NoPageFound</div>
-    )
-}
-
-export default NoPageFound
