23784d298d9528448f624b0b2628fbfd64cf8182
Aman Kumar <aman.kumar2k15gmail.com>
2024-04-04 14:59:47 +0530

    changes of audit report

--- src/data/dataSet.jsx
+++ src/data/dataSet.jsx
@@ -1,14 +1,3 @@
-import React from 'react';
-
-import {
-    FacebookShareButton,
-    FacebookIcon,
-    TwitterShareButton,
-    TwitterIcon,
-    LinkedinShareButton,
-    LinkedinIcon
-} from 'react-share';
-
 export const socialIcon = [
     {
         icon: "/img/navbar/Linkdin.png",
@@ -25,26 +14,4 @@ export const socialIcon = [
 
 ];
 
-export const HomeSocialIcon = [
-    {
-        icon: (
-            <LinkedinShareButton url='https://www.linkedin.com/company/cutechsolutions/'>
-                <LinkedinIcon size={35} round={false} />
-            </LinkedinShareButton>
-        )
-    },
-    {
-        icon: (
-            <TwitterShareButton url='https://twitter.com/Cutech_Solution'>
-                <TwitterIcon size={35} round={false} />
-            </TwitterShareButton>
-        )
-    },
-    {
-        icon: (
-            <FacebookShareButton url='https://www.facebook.com/profile.php?id=61557138917291'>
-                <FacebookIcon size={35} round={false} />
-            </FacebookShareButton>
-        )
-    }
-];
+
